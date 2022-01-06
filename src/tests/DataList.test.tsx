5439e99726c280f615caf0333e075d605d51037b
Demi <verussapientia@gmail.com>
2022-01-06 17:20:49 -0500

    fixed Jest tests

--- src/tests/DataList.test.tsx
+++ src/tests/DataList.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import App from '../App';
+import { DataList } from '../components/DataList';
 import * as ReactDOM from 'react-dom';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<DataList data={[]} />, div);
 });
